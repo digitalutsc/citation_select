454d80a65e9162c9c160cf4f07dd1f01b9bb83f1
Jeff He <jeffbuyunhe@hotmail.com>
2023-10-12 16:47:23 -0400

    fixed bug with citations not being made

--- src/Plugin/CitationFieldFormatter/EdtfDateFormatter.php
+++ src/Plugin/CitationFieldFormatter/EdtfDateFormatter.php
@@ -21,11 +21,25 @@ class EdtfDateFormatter extends CitationFieldFormatterBase {
   protected function parseDate($string) {
     $parser = EdtfFactory::newParser();
     $edtf_value = $parser->parse($string)->getEdtfValue();
-    $date_parts = [
-      $edtf_value->getYear(),
-      $edtf_value->getMonth(),
-      $edtf_value->getDay(),
-    ];
+
+    // The parser may return either an EDTF Set or an ExtDate object.
+    if (method_exists($edtf_value, 'getDates')) {
+      // Parser returned a Set.
+      $edtf_dates = $edtf_value = $edtf_value->getDates();
+      $date_parts = [
+        $edtf_dates[0]->getYear(),
+        $edtf_dates[0]->getMonth(),
+        $edtf_dates[0]->getDay(),
+      ];
+    }
+    else {
+      // Parser returned an ExtDate object.
+      $date_parts = [
+        $edtf_value->getYear(),
+        $edtf_value->getMonth(),
+        $edtf_value->getDay(),
+      ];
+    }
 
     return [
       'date-parts' => [$date_parts],
